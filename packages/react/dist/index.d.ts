bdef289dd991ccd44b20e72fb3b48d619db583e1
Eduardo Winter <eduardowb@gmail.com>
2022-11-03 08:12:04 -0300

    fix: yml file

--- packages/react/dist/index.d.ts
+++ packages/react/dist/index.d.ts
@@ -6,77 +6,13 @@ import * as Avatar$1 from '@radix-ui/react-avatar';
 import * as Checkbox$1 from '@radix-ui/react-checkbox';
 
 declare const Box: _stitches_react_types_styled_component.StyledComponent<"div", {}, {}, _stitches_react_types_css_util.CSS<{}, {
-    colors: {
-        white: string;
-        black: string;
-        gray100: string;
-        gray200: string;
-        gray400: string;
-        gray500: string;
-        gray600: string;
-        gray700: string;
-        gray800: string;
-        gray900: string;
-        green300: string;
-        green500: string;
-        green700: string;
-        green900: string;
-    };
-    fontSizes: {
-        xxs: string;
-        xs: string;
-        sm: string;
-        md: string;
-        lg: string;
-        xl: string;
-        '2xl': string;
-        '4xl': string;
-        '5xl': string;
-        '6xl': string;
-        '7xl': string;
-        '8xl': string;
-        '9xl': string;
-    };
-    fontWeights: {
-        regular: string;
-        medium: string;
-        bold: string;
-    };
-    fonts: {
-        default: string;
-        code: string;
-    };
-    lineHeights: {
-        shorter: string;
-        short: string;
-        base: string;
-        tall: string;
-    };
-    radii: {
-        px: string;
-        xs: string;
-        sm: string;
-        md: string;
-        lg: string;
-        full: string;
-    };
-    space: {
-        1: string;
-        2: string;
-        3: string;
-        4: string;
-        5: string;
-        6: string;
-        7: string;
-        8: string;
-        10: string;
-        12: string;
-        16: string;
-        20: string;
-        40: string;
-        64: string;
-        80: string;
-    };
+    colors: unknown;
+    fontSizes: unknown;
+    fontWeights: unknown;
+    fonts: unknown;
+    lineHeights: unknown;
+    radii: unknown;
+    space: unknown;
 }, {
     height: "space";
     width: "space";
@@ -214,77 +150,13 @@ interface BoxProps extends ComponentProps<typeof Box> {
 declare const Text: _stitches_react_types_styled_component.StyledComponent<"p", {
     size?: "xxs" | "xs" | "sm" | "md" | "lg" | "xl" | "2xl" | "4xl" | "5xl" | "6xl" | "7xl" | "8xl" | "9xl" | undefined;
 }, {}, _stitches_react_types_css_util.CSS<{}, {
-    colors: {
-        white: string;
-        black: string;
-        gray100: string;
-        gray200: string;
-        gray400: string;
-        gray500: string;
-        gray600: string;
-        gray700: string;
-        gray800: string;
-        gray900: string;
-        green300: string;
-        green500: string;
-        green700: string;
-        green900: string;
-    };
-    fontSizes: {
-        xxs: string;
-        xs: string;
-        sm: string;
-        md: string;
-        lg: string;
-        xl: string;
-        '2xl': string;
-        '4xl': string;
-        '5xl': string;
-        '6xl': string;
-        '7xl': string;
-        '8xl': string;
-        '9xl': string;
-    };
-    fontWeights: {
-        regular: string;
-        medium: string;
-        bold: string;
-    };
-    fonts: {
-        default: string;
-        code: string;
-    };
-    lineHeights: {
-        shorter: string;
-        short: string;
-        base: string;
-        tall: string;
-    };
-    radii: {
-        px: string;
-        xs: string;
-        sm: string;
-        md: string;
-        lg: string;
-        full: string;
-    };
-    space: {
-        1: string;
-        2: string;
-        3: string;
-        4: string;
-        5: string;
-        6: string;
-        7: string;
-        8: string;
-        10: string;
-        12: string;
-        16: string;
-        20: string;
-        40: string;
-        64: string;
-        80: string;
-    };
+    colors: unknown;
+    fontSizes: unknown;
+    fontWeights: unknown;
+    fonts: unknown;
+    lineHeights: unknown;
+    radii: unknown;
+    space: unknown;
 }, {
     height: "space";
     width: "space";
@@ -422,77 +294,13 @@ interface TextProps extends ComponentProps<typeof Text> {
 declare const Heading: _stitches_react_types_styled_component.StyledComponent<"h2", {
     size?: "sm" | "md" | "lg" | "2xl" | "4xl" | "5xl" | "6xl" | "3xl" | undefined;
 }, {}, _stitches_react_types_css_util.CSS<{}, {
-    colors: {
-        white: string;
-        black: string;
-        gray100: string;
-        gray200: string;
-        gray400: string;
-        gray500: string;
-        gray600: string;
-        gray700: string;
-        gray800: string;
-        gray900: string;
-        green300: string;
-        green500: string;
-        green700: string;
-        green900: string;
-    };
-    fontSizes: {
-        xxs: string;
-        xs: string;
-        sm: string;
-        md: string;
-        lg: string;
-        xl: string;
-        '2xl': string;
-        '4xl': string;
-        '5xl': string;
-        '6xl': string;
-        '7xl': string;
-        '8xl': string;
-        '9xl': string;
-    };
-    fontWeights: {
-        regular: string;
-        medium: string;
-        bold: string;
-    };
-    fonts: {
-        default: string;
-        code: string;
-    };
-    lineHeights: {
-        shorter: string;
-        short: string;
-        base: string;
-        tall: string;
-    };
-    radii: {
-        px: string;
-        xs: string;
-        sm: string;
-        md: string;
-        lg: string;
-        full: string;
-    };
-    space: {
-        1: string;
-        2: string;
-        3: string;
-        4: string;
-        5: string;
-        6: string;
-        7: string;
-        8: string;
-        10: string;
-        12: string;
-        16: string;
-        20: string;
-        40: string;
-        64: string;
-        80: string;
-    };
+    colors: unknown;
+    fontSizes: unknown;
+    fontWeights: unknown;
+    fonts: unknown;
+    lineHeights: unknown;
+    radii: unknown;
+    space: unknown;
 }, {
     height: "space";
     width: "space";
@@ -628,77 +436,13 @@ interface HeadingProps extends ComponentProps<typeof Heading> {
 }
 
 declare const AvatarImage: _stitches_react_types_styled_component.StyledComponent<react.ForwardRefExoticComponent<Avatar$1.AvatarImageProps & react.RefAttributes<HTMLImageElement>>, {}, {}, _stitches_react_types_css_util.CSS<{}, {
-    colors: {
-        white: string;
-        black: string;
-        gray100: string;
-        gray200: string;
-        gray400: string;
-        gray500: string;
-        gray600: string;
-        gray700: string;
-        gray800: string;
-        gray900: string;
-        green300: string;
-        green500: string;
-        green700: string;
-        green900: string;
-    };
-    fontSizes: {
-        xxs: string;
-        xs: string;
-        sm: string;
-        md: string;
-        lg: string;
-        xl: string;
-        '2xl': string;
-        '4xl': string;
-        '5xl': string;
-        '6xl': string;
-        '7xl': string;
-        '8xl': string;
-        '9xl': string;
-    };
-    fontWeights: {
-        regular: string;
-        medium: string;
-        bold: string;
-    };
-    fonts: {
-        default: string;
-        code: string;
-    };
-    lineHeights: {
-        shorter: string;
-        short: string;
-        base: string;
-        tall: string;
-    };
-    radii: {
-        px: string;
-        xs: string;
-        sm: string;
-        md: string;
-        lg: string;
-        full: string;
-    };
-    space: {
-        1: string;
-        2: string;
-        3: string;
-        4: string;
-        5: string;
-        6: string;
-        7: string;
-        8: string;
-        10: string;
-        12: string;
-        16: string;
-        20: string;
-        40: string;
-        64: string;
-        80: string;
-    };
+    colors: unknown;
+    fontSizes: unknown;
+    fontWeights: unknown;
+    fonts: unknown;
+    lineHeights: unknown;
+    radii: unknown;
+    space: unknown;
 }, {
     height: "space";
     width: "space";
@@ -841,77 +585,13 @@ declare const Button: _stitches_react_types_styled_component.StyledComponent<"bu
     variant?: "primary" | "secondary" | "tertiary" | undefined;
     size?: "sm" | "md" | undefined;
 }, {}, _stitches_react_types_css_util.CSS<{}, {
-    colors: {
-        white: string;
-        black: string;
-        gray100: string;
-        gray200: string;
-        gray400: string;
-        gray500: string;
-        gray600: string;
-        gray700: string;
-        gray800: string;
-        gray900: string;
-        green300: string;
-        green500: string;
-        green700: string;
-        green900: string;
-    };
-    fontSizes: {
-        xxs: string;
-        xs: string;
-        sm: string;
-        md: string;
-        lg: string;
-        xl: string;
-        '2xl': string;
-        '4xl': string;
-        '5xl': string;
-        '6xl': string;
-        '7xl': string;
-        '8xl': string;
-        '9xl': string;
-    };
-    fontWeights: {
-        regular: string;
-        medium: string;
-        bold: string;
-    };
-    fonts: {
-        default: string;
-        code: string;
-    };
-    lineHeights: {
-        shorter: string;
-        short: string;
-        base: string;
-        tall: string;
-    };
-    radii: {
-        px: string;
-        xs: string;
-        sm: string;
-        md: string;
-        lg: string;
-        full: string;
-    };
-    space: {
-        1: string;
-        2: string;
-        3: string;
-        4: string;
-        5: string;
-        6: string;
-        7: string;
-        8: string;
-        10: string;
-        12: string;
-        16: string;
-        20: string;
-        40: string;
-        64: string;
-        80: string;
-    };
+    colors: unknown;
+    fontSizes: unknown;
+    fontWeights: unknown;
+    fonts: unknown;
+    lineHeights: unknown;
+    radii: unknown;
+    space: unknown;
 }, {
     height: "space";
     width: "space";
@@ -1047,77 +727,13 @@ interface ButtonProps extends ComponentProps<typeof Button> {
 }
 
 declare const Input: _stitches_react_types_styled_component.StyledComponent<"input", {}, {}, _stitches_react_types_css_util.CSS<{}, {
-    colors: {
-        white: string;
-        black: string;
-        gray100: string;
-        gray200: string;
-        gray400: string;
-        gray500: string;
-        gray600: string;
-        gray700: string;
-        gray800: string;
-        gray900: string;
-        green300: string;
-        green500: string;
-        green700: string;
-        green900: string;
-    };
-    fontSizes: {
-        xxs: string;
-        xs: string;
-        sm: string;
-        md: string;
-        lg: string;
-        xl: string;
-        '2xl': string;
-        '4xl': string;
-        '5xl': string;
-        '6xl': string;
-        '7xl': string;
-        '8xl': string;
-        '9xl': string;
-    };
-    fontWeights: {
-        regular: string;
-        medium: string;
-        bold: string;
-    };
-    fonts: {
-        default: string;
-        code: string;
-    };
-    lineHeights: {
-        shorter: string;
-        short: string;
-        base: string;
-        tall: string;
-    };
-    radii: {
-        px: string;
-        xs: string;
-        sm: string;
-        md: string;
-        lg: string;
-        full: string;
-    };
-    space: {
-        1: string;
-        2: string;
-        3: string;
-        4: string;
-        5: string;
-        6: string;
-        7: string;
-        8: string;
-        10: string;
-        12: string;
-        16: string;
-        20: string;
-        40: string;
-        64: string;
-        80: string;
-    };
+    colors: unknown;
+    fontSizes: unknown;
+    fontWeights: unknown;
+    fonts: unknown;
+    lineHeights: unknown;
+    radii: unknown;
+    space: unknown;
 }, {
     height: "space";
     width: "space";
@@ -1258,77 +874,13 @@ declare namespace TextInput {
 }
 
 declare const TextArea: _stitches_react_types_styled_component.StyledComponent<"textarea", {}, {}, _stitches_react_types_css_util.CSS<{}, {
-    colors: {
-        white: string;
-        black: string;
-        gray100: string;
-        gray200: string;
-        gray400: string;
-        gray500: string;
-        gray600: string;
-        gray700: string;
-        gray800: string;
-        gray900: string;
-        green300: string;
-        green500: string;
-        green700: string;
-        green900: string;
-    };
-    fontSizes: {
-        xxs: string;
-        xs: string;
-        sm: string;
-        md: string;
-        lg: string;
-        xl: string;
-        '2xl': string;
-        '4xl': string;
-        '5xl': string;
-        '6xl': string;
-        '7xl': string;
-        '8xl': string;
-        '9xl': string;
-    };
-    fontWeights: {
-        regular: string;
-        medium: string;
-        bold: string;
-    };
-    fonts: {
-        default: string;
-        code: string;
-    };
-    lineHeights: {
-        shorter: string;
-        short: string;
-        base: string;
-        tall: string;
-    };
-    radii: {
-        px: string;
-        xs: string;
-        sm: string;
-        md: string;
-        lg: string;
-        full: string;
-    };
-    space: {
-        1: string;
-        2: string;
-        3: string;
-        4: string;
-        5: string;
-        6: string;
-        7: string;
-        8: string;
-        10: string;
-        12: string;
-        16: string;
-        20: string;
-        40: string;
-        64: string;
-        80: string;
-    };
+    colors: unknown;
+    fontSizes: unknown;
+    fontWeights: unknown;
+    fonts: unknown;
+    lineHeights: unknown;
+    radii: unknown;
+    space: unknown;
 }, {
     height: "space";
     width: "space";
@@ -1463,77 +1015,13 @@ interface TextAreaProps extends ComponentProps<typeof TextArea> {
 }
 
 declare const CheckboxContainer: _stitches_react_types_styled_component.StyledComponent<react.ForwardRefExoticComponent<Checkbox$1.CheckboxProps & react.RefAttributes<HTMLButtonElement>>, {}, {}, _stitches_react_types_css_util.CSS<{}, {
-    colors: {
-        white: string;
-        black: string;
-        gray100: string;
-        gray200: string;
-        gray400: string;
-        gray500: string;
-        gray600: string;
-        gray700: string;
-        gray800: string;
-        gray900: string;
-        green300: string;
-        green500: string;
-        green700: string;
-        green900: string;
-    };
-    fontSizes: {
-        xxs: string;
-        xs: string;
-        sm: string;
-        md: string;
-        lg: string;
-        xl: string;
-        '2xl': string;
-        '4xl': string;
-        '5xl': string;
-        '6xl': string;
-        '7xl': string;
-        '8xl': string;
-        '9xl': string;
-    };
-    fontWeights: {
-        regular: string;
-        medium: string;
-        bold: string;
-    };
-    fonts: {
-        default: string;
-        code: string;
-    };
-    lineHeights: {
-        shorter: string;
-        short: string;
-        base: string;
-        tall: string;
-    };
-    radii: {
-        px: string;
-        xs: string;
-        sm: string;
-        md: string;
-        lg: string;
-        full: string;
-    };
-    space: {
-        1: string;
-        2: string;
-        3: string;
-        4: string;
-        5: string;
-        6: string;
-        7: string;
-        8: string;
-        10: string;
-        12: string;
-        16: string;
-        20: string;
-        40: string;
-        64: string;
-        80: string;
-    };
+    colors: unknown;
+    fontSizes: unknown;
+    fontWeights: unknown;
+    fonts: unknown;
+    lineHeights: unknown;
+    radii: unknown;
+    space: unknown;
 }, {
     height: "space";
     width: "space";
